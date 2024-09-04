5c605fd341e86f3fb76a3f5638269901bf8e2b2f
najmul905 <nh4808563@gmail.com>
2024-09-04 16:08:18 +0600

    add to mongoDB database

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -10,7 +19,33 @@ const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express_1.default.json());
-app.get("/", (req, res) => (res.send("Connect with typescript")));
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_Name}:${process.env.DB_Pass}@cluster0.yq5wikg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            // Connect the client to the server	(optional starting in v4.7)
+            yield client.connect();
+            // Send a ping to confirm a successful connection
+            yield client.db("admin").command({ ping: 1 });
+            console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        }
+        finally {
+            // Ensures that the client will close when you finish/error
+            // await client.close();
+        }
+    });
+}
+run().catch(console.dir);
+app.get("/", (req, res) => (res.send("type script add in node and express")));
 app.listen(port, () => {
     console.log(`server is running now ${port}`);
 });
